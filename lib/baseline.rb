87b61fa25970f2fb3cf506caf7f163b106d2b916
David Susco <dsusco@clarku.edu>
2023-01-24 10:43:48 -0500

    added rails support

--- lib/baseline.rb
+++ lib/baseline.rb
@@ -1,13 +1,24 @@
-begin
-  if Jekyll
+module Baseline
+  if defined?(Jekyll)
     require 'hash'
 
     require 'baseline/filters'
     require 'baseline/hooks'
     require 'baseline/tags'
   end
-rescue
-end
 
-module Baseline
+  if defined?(Rails) && defined?(Rails::Engine)
+    class Engine < ::Rails::Engine
+      initializer 'baseline.paths', group: :all do |app|
+        app.config.assets.paths << File.expand_path('../_sass', __dir__)
+      end
+    end
+  else
+    begin
+      require 'sass'
+
+      Sass.load_paths << File.expand_path('../_sass', __dir__)
+    rescue LoadError
+    end
+  end
 end
